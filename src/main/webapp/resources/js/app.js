da8a8ddd17ec2b26b2b2f045a94e080c17f3f05f
zdornov <maks000156@yandex.ru>
2019-09-05 14:08:11 +0400

    ExtJS

---
+++ src/main/webapp/resources/js/app.js
@@ -0,0 +1,96 @@
+Ext.application({
+    name: 'CarCatalog',
+
+    views: [
+        'AddCarFormView',
+        'CarCatalogView',
+        'CarGridView',
+        'SearchCarView'
+    ],
+
+    launch: function () {
+        Ext.create('Ext.container.Viewport', {
+            layout: 'fit',
+            items: {
+                xtype  : 'carCatalogView'
+            }
+        });
+    }
+});
+
+Ext.application({
+    name: 'CarCatalog',
+    launch: function () {
+        Ext.create('Ext.container.Viewport', {
+            layout: 'fit',
+            items: {
+                xtype: 'panel',
+                html: '<h2>Каталог автомобилей</h2>'
+            }
+        });
+    }
+});
+Ext.define('CarCatalog.view.CarGridView', {
+    extend: 'Ext.grid.Panel',
+    alias: 'widget.carGridView',
+    width: 400,
+    height: 300,
+    frame: true,
+    iconCls: 'icon-user',
+    viewConfig:{
+        markDirty:false
+    },
+    columns: [
+        {
+            text: 'Модель',
+            flex: 1,
+            sortable: true,
+            dataIndex: 'name',
+            editor: {
+                xtype:'textfield',
+                allowBlank: false,
+                blankText: 'Это поле должно быть заполнено'
+            }
+        },
+        {
+            flex: 2,
+            text: 'Цена',
+            sortable: true,
+            dataIndex: 'price',
+            editor: {
+                xtype:'textfield',
+                regex: /^([0-9]{1,20})*$/,
+                regexText: 'Цена должна состоять из цифр',
+                allowBlank: false,
+                blankText: 'Это поле должно быть заполнено'
+            }
+        }
+    ],
+    plugins: [
+        Ext.create('Ext.grid.plugin.RowEditing', {
+            clicksToEdit: 2,
+            saveBtnText: 'Сохранить',
+            cancelBtnText: 'Отменить'
+        })
+    ],
+    selType: 'rowmodel',
+    dockedItems: [
+        {
+            xtype: 'toolbar',
+            items: [
+                {
+                    text: 'Добавить',
+                    action: 'add',
+                    iconCls: 'icon-add'
+                },
+                '-',
+                {
+                    action: 'delete',
+                    text: 'Удалить',
+                    iconCls: 'icon-delete',
+                    disabled: true
+                }
+            ]
+        }
+    ]
+});
--- src/main/webapp/views/mainPage.jsp
+++ src/main/webapp/views/mainPage.jsp
@@ -8,24 +8,41 @@
 <html>
 <head>
     <title>title</title>
+    <script src="${contextPath}/resources/extjs/ext-all.js" type="text/javascript"></script>
+    <link href="${contextPath}/resources/extjs/resources/css/ext-all.css" rel="stylesheet">
+    <script type="text/javascript" src="${contextPath}/resources/js/app.js"></script>
+    <script>
+        Ext.create('Ext.Panel', {
+            layout: 'hbox', // важно
+            renderTo: Ext.getBody(),
+            items: [
+                { xtype: 'button', text: 'Select' },
+                { xtype: 'container', flex:1 },        // <== magic )
+                { xtype: 'button', text: 'Advanced options' },
+                { xtype: 'button', text: 'Back' }
+            ]
+        })
+
+
+    </script>
 </head>
 <body>
-<h1>Добро пожаловать! </h1>
 <ul>
-    <button style="float:right" class="button" type="submit" onclick="document.forms['logoutForm'].submit()">Выйти
-    </button>
+    <button style="float:right" class="button" onclick="document.forms['logoutForm'].submit()">Выйти    </button>
 
     <c:if test="${pageContext.request.userPrincipal.name != null}">
     <form id="logoutForm" method="POST" action="${contextPath}/logout">
         <input type="hidden" name="${_csrf.parameterName}" value="${_csrf.token}"/>
     </form>
-    <p class="userInput" align="right"> Вы вошли как: ${pageContext.request.userPrincipal.name}
+    <p class="userInput" align="right"> Вы вошли как: ${pageContext.request.userPrincipal.name}</p>
+
         </c:if>
         <c:forEach items="${index}" var="item" varStatus="status">
         <tr>
             <h2> <a href="${contextPath}/profile/${item.key}">${item.value}</a></h2>
         </tr>
         </c:forEach>
+
 
 
 </ul>
